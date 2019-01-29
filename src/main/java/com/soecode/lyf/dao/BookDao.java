36d5f69211a8104f380de5b3a9d174ff569abaae
DLW3D <787530307@qq.com>
2019-01-29 19:12:31 +0800

    添加注释

--- src/main/java/com/soecode/lyf/dao/BookDao.java
+++ src/main/java/com/soecode/lyf/dao/BookDao.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.annotations.Param;
 import com.soecode.lyf.entity.Book;
 
 public interface BookDao {
+	// 提示：这里为什么要给方法的参数添加@Param注解呢？是因为该方法有两个或以上的参数，一定要加，不然mybatis识别不了。
+	// 上面的BookDao接口的queryById方法和reduceNumber方法只有一个参数book_id，所以可以不用加 @Param注解，当然加了也无所谓~
 
 	/**
 	 * 通过ID查询单本图书
--- src/test/java/com/soecode/lyf/BaseTest.java
+++ src/test/java/com/soecode/lyf/BaseTest.java
@@ -6,6 +6,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
+ *
+ * 在测试前需要让程序读入spring-dao和mybatis等配置文件
+ * 所以我这里就抽离出来一个BaseTest类，只要是测试方法就继承它，这样那些繁琐的重复的代码就不用写那么多了
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 // 告诉junit spring配置文件
